ea56eebedd621c71e375133be8bc19f0f14b2bd7
Anton Barouka <borovko28@gmail.com>
2022-06-05 01:29:34 +0300

    feat: compres-decompress part of task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+const pipe = promisify(pipeline);
+
 export const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = createGzip();
+    const source = createReadStream("./files/fileToCompress.txt");
+    const destination = createWriteStream("archive.gz");
+    await pipe(source, gzip, destination);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,17 @@
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+const pipe = promisify(pipeline);
+
 export const decompress = async () => {
-    // Write your code here 
+  try {
+    const gunzip = createGunzip();
+    const source = createReadStream("archive.gz");
+    const destination = createWriteStream("./fileToCompress.txt");
+    await pipe(source, gunzip, destination);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
+decompress();
